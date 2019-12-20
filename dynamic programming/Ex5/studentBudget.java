4cf164c418b58d622ecf6cbf79e80937a46a4826
Farsene1 <33878402+Farsene1@users.noreply.github.com>
2019-12-20 18:53:16 +0100

    Create studentBudget.java

---
+++ dynamic programming/Ex5/studentBudget.java
@@ -0,0 +1,17 @@
+package weblab;
+
+import java.io.*;
+import java.util.*;
+
+class Solution {
+
+  // inputstream.
+  public static String run(InputStream in) {
+    return new Solution().solve(in);
+  }
+
+  public String solve(InputStream in) {
+  // TODO
+  }
+}
+
